78b34bfb38d997d0744616e744f0b6ad6fe94ccd
DavydovaLS <lusyadavydova1984@gmail.com>
2024-06-02 12:38:50 +0300

    домашнее задание 4

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const groceries = document.querySelector('.groceries');
+const inputForm = document.querySelector('#input');
+const labelForm = document.querySelector('#items');
+
+labelForm.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const labelText = inputForm.value;
+        const newItem = documentCreateElement('div');
+        newItem.classList.add('items');
+        newItem.textContent = labelText;
+
+        if (labelText != '') {
+            groceries.append(newItem);
+        }
+    inputForm.value = '';
+    }
+});
+
+newItem.addEventListener('click', function() {
+    newItem.classList.toggle('done');
+});
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
